ab979d486e1f1d716f8196d82fd4113d2f97ecb1
Robert Glaser <mail@robert-glaser.de>
2012-01-10 18:52:38 +0100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpdyTest::Application.config.secret_token = '397845da1ed8e628200028e43a225496a85923c4bb51ce464bb76697b18f6cf0892d70e1491fa85dd368fb634545b803f6a3635a0269731f54c4f1af2a4b2865'
